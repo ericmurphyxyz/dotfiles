012f1af354e2f2ddf4d4369e5d8ab40598ce5b96
Eric Murphy <eric@ericmurphy.xyz>
2020-04-29 17:46:34 +0700

    Polybar first draft

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar main &
+#polybar bot
+
+echo "Bars launched..."

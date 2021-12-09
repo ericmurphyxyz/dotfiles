03423a7f27e4f73ecd5e38b0bfa519bb5087a386
ericmurphyxyz <eric@ericmurphy.xyz>
2021-12-09 12:15:45 +0700

    Change polybar theme and layout

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+killall polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-polybar main &
-#polybar bot
+# Launch polybar
+polybar main -c $(dirname $0)/config.ini &
 
-echo "Bars launched..."
+if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
+	polybar external -c $(dirname $0)/config.ini &
+fi

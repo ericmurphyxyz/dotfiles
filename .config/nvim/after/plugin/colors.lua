bbb67588baf962c58384843b6432f4ba184f4e45
ericmurphyxyz <eric@ericmurphy.xyz>
2023-01-31 14:19:09 +0700

    Fix transparency issues with nvim

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -2,3 +2,5 @@ vim.cmd [[colorscheme tokyonight-night]]
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = "none" })
